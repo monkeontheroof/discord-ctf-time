2dd9b633440583bb69e4720c1a7c78cfbd686571
Khang <khangkhoailang20@gmail.com>
2024-07-04 15:55:46 +0700

    today

--- ctf_bot.py
+++ ctf_bot.py
@@ -5,17 +5,23 @@ from requests.exceptions import HTTPError
 from dateutil import parser
 from discord.ext import commands
 from discord import app_commands
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Retrieve the bot token from environment variables
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True  # Enable message content intent
 
-# Create a bot instance with a command prefix
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'We have logged in as {bot.user}')
-    # Register the slash commands when the bot is ready
     await bot.tree.sync()
 
 @bot.tree.command(name="upcoming", description="Get upcoming CTF events")
@@ -28,17 +34,14 @@ async def upcoming(interaction: discord.Interaction, limit: int):
     try:
         ctf_events = get_ctf_events(limit)
         if ctf_events:
-            # Reply with the first message
             await interaction.response.send_message("Here are the upcoming CTF events:", ephemeral=True)
 
-            # Follow up with embeds
             for embed in ctf_events:
                 await interaction.followup.send(embed=embed)
         else:
             await interaction.response.send_message('Could not fetch CTF events.', ephemeral=True)
 
     except Exception as e:
-        # Handle exceptions and send an error message
         await interaction.response.send_message(f'An error occurred: {e}', ephemeral=True)
 
 def get_ctf_events(limit=5):
@@ -75,9 +78,7 @@ def get_ctf_events(limit=5):
         return None
 
 def format_datetime(datetime_str):
-    # Parse the datetime string with timezone information
     datetime_obj = parser.isoparse(datetime_str)
-    # Format the datetime object to the desired format
     return datetime_obj.strftime("%d-%m-%Y %H:%M:%S")
 
-bot.run('MTI1ODMxMDIyMTgxODg5MjI5OQ.GMnlgq.d-rejWSWNWgQRik22os9m_egnhO_jspuJKn1_I')
+bot.run(DISCORD_TOKEN)
